da9fb41024e463cda83fa542317de222170f0fc2
Jon Saints <saintsjd@gmail.com>
2016-01-31 01:50:18 -0700

    require everything to be served from dist folder

---
+++ Procfile
@@ -0,0 +1,9 @@
+# Production
+web: node dist/server.js
+
+# Development
+build-frontend-development: npm run build-frontend-development
+build-server: npm run build-server
+build-data: npm run build-data
+build-webpack-config: npm run build-webpack-config
+dev-server: npm run dev-server
--- server.js
+++ server.js
@@ -8,45 +8,14 @@ import {Schema} from './data/schema';
 const APP_PORT = 3000;
 const GRAPHQL_PORT = 8080;
 
-if (process.env.NODE_ENV === 'production') {
-    var app = express();
-    app.use('/graphql', graphQLHTTP({
-      schema: Schema,
-    }));
+var app = express();
+app.use('/graphql', graphQLHTTP({
+  schema: Schema,
+}));
 
-    // Serve static resources
-    app.use('/', express.static('public'));
-    app.use('/js', express.static('dist/js'));
-    app.listen(process.env.PORT || APP_PORT, () => {
-      console.log(`App is now running on http://localhost:${process.env.PORT || APP_PORT}`);
-    });
-} else {
-    // Expose a GraphQL endpoint
-    var graphQLServer = express();
-    graphQLServer.use('/', graphQLHTTP({
-      graphiql: true,
-      pretty: true,
-      schema: Schema,
-    }));
-    graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-      `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}`
-    ));
-
-    // Serve the Relay app
-    var config = require('./webpack.config');
-    var compiler = webpack(config);
-    var app = new WebpackDevServer(compiler, {
-      noInfo: true,
-      hot: true,
-      historyApiFallback: true,
-      contentBase: '/public/',
-      proxy: {'/graphql': `http://localhost:${GRAPHQL_PORT}`},
-      publicPath: '/js/',
-      stats: {colors: true}
-    });
-    // Serve static resources
-    app.use('/', express.static(path.resolve(__dirname, 'public')));
-    app.listen(APP_PORT, () => {
-      console.log(`App is now running on http://localhost:${APP_PORT}/webpack-dev-server`);
-    });
-}
+// Serve static resources
+app.use('/', express.static('public'));
+app.use('/js', express.static('dist/js'));
+app.listen(process.env.PORT || APP_PORT, () => {
+  console.log(`App is now running on http://localhost:${process.env.PORT || APP_PORT}`);
+});
